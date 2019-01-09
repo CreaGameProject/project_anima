c78f799d168b2895e66c3e3ed1a5b819bed51369
Omori <39805387+OmoriItsuki@users.noreply.github.com>
2019-01-10 02:20:24 +0900

    統一バージョンに対応

---
+++ Anima/Assets/Scripts/EnSRorSGbutton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EnSRorSGbutton : MonoBehaviour {
+
+    public GameObject canvas;
+    public GameObject SR;
+    public GameObject SG;
+
+	public void SRButton()
+    {
+        canvas.SetActive(false);
+        SR.SetActive(true);
+    }
+
+    public void SGButton()
+    {
+        canvas.SetActive(false);
+        SG.SetActive(true);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("WeaponManager");
+    }
+
+    public void Back2()
+    {
+        SG.SetActive(false);
+        SR.SetActive(false);
+        canvas.SetActive(true);
+    }
+}
