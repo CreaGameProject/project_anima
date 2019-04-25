4ad22bf4665489cb36e665feee5305ded37ae5bf
OmoriItsuki <39805387+kinoko555@users.noreply.github.com>
2019-04-25 17:56:34 +0900

    変更追加

---
+++ Anima/Assets/omori/Scripts/EnOrSeButtons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EnOrSeButtons : MonoBehaviour
+{
+    
+
+    public void Onclick()
+    {
+        Debug.Log("EnhanceButton clicked");
+        SceneManager.LoadScene("WeaponEnhance");
+    }
+    public void Onclick2()
+    {
+        SceneManager.LoadScene("WeaponSelect");
+    }
+
+    public void Onclick3()
+    {
+
+        //シーン"home"の名前を一致させてね
+        SceneManager.LoadScene("Title");
+    }
+}
---
+++ Anima/Assets/omori/Scripts/GetData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetData : MonoBehaviour
+{
+    private Data data;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        data = GameObject.Find("GameManager").GetComponent<Data>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int a;
+        a=data.weapon.
+    }
+}
